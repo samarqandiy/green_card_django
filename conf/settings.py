593bab4a84a8f859ddea31d16aa01cc5def229ce
JBN-23 <69708014+JBN-23@users.noreply.github.com>
2022-07-14 22:25:16 +0500

    hi

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@fv@%86-^*!8bir-l^(g!uo+#6ldew3ul$6ur*ixfibyrs7hs3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '3.88.159.170']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '3.88.159.170']
 
 
 # Application definition
