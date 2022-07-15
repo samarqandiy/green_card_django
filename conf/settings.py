5fbe088ca5dd4aaacef0877c8e776477aa824e86
JBN-23 <69708014+JBN-23@users.noreply.github.com>
2022-07-15 10:25:24 +0500

    hi

--- conf/settings.py
+++ conf/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     #     # 'ENGINE': 'django.db.backends.sqlite3',
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     #     'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # }
 }
 
 
